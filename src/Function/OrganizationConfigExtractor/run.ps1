1f9f4ccd1fc82b16c5baef219c20cfe630dd814b
John Gemkow <jogemkow@microsoft.com>
2022-08-30 18:11:47 -0500

    Updated Org Config Extractor with working script

--- src/Function/OrganizationConfigExtractor/run.ps1
+++ src/Function/OrganizationConfigExtractor/run.ps1
@@ -6,13 +6,19 @@ if ($Timer.IsPastDue) {
     Write-Warning "Trigger for OrganizationConfigExtractor is running late."
 }
 
+# Workaround for https://github.com/Azure/azure-powershell/issues/14416
+Import-Module Az.Storage -Force
+
 # Retrieve variables for Azure Function
 $certificateName = $env:CertificateName
 $clientIdSecretName = $env:ClientIdSecretName
 $keyVaultName = $env:KeyVaultName
-$splunkHECEndpointSecretName = $env:SplunkHECEndpointSecretName
-$splunkTokenSecretName = $env:SplunkEXOConfigTokenSecretName
 $tenantNameSecretName = $env:TenantNameSecretName
+$orgConfigExportsContainerName = $env:OrgConfigExportsContainerName
+$storageAccountConnectionString = $env:AzureWebJobsStorage # Storage Account connection string
+
+$fileName = "EXOOrgConfig_$(get-date -f yyMMddhhmmss).json"
+
 # Retrive information from Key Vault
 try {
 	# Obtain certificate from Key Vault
@@ -23,8 +29,6 @@ try {
 	# Obtain client ID, tenant name, Splunk Host Name, and Splunk Token from Key Vault
 	$clientId = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $clientIdSecretName -AsPlainText
 	$tenantName = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $tenantNameSecretName -AsPlainText
-	$splunkHECEndpoint = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $splunkHECEndpointSecretName -AsPlainText
-	$splunkToken = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $splunkTokenSecretName -AsPlainText
 	
 	Write-Verbose "Secrets and certificate retrieved."
 } catch {
@@ -36,14 +40,14 @@ try {
 try {
 	Write-Output "Connecting to EXO using AAD application service principal details from Key Vault..."
 	$certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]([System.Convert]::FromBase64String($certValue))
-	Connect-ExchangeOnline -Certificate $certificate -AppId $clientId -Organization $tenantName
+	Connect-ExchangeOnline -Certificate $certificate -AppId $clientId -Organization $tenantName -CommandName "Get-OrganizationConfig" -ShowBanner:$false
 	Write-Output "Connected to EXO using AAD application service principal details from Key Vault."
 } catch {
 	Write-Error -Message "Failed to connect to EXO." -Category ConnectionError
 	throw $_
 }
 
-# Gather message tracking logs
+# Gather prg config logs
 Write-Output "Starting to gather EXO config"
 $orgConfig = $null
 try {
@@ -54,16 +58,27 @@ try {
 } finally {
 	Disconnect-ExchangeOnline -Confirm:$false
 }
-Write-Output "Gathered organization config. Preparing to send to Splunk."
 
-# Send config to HEC
-#[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true} # Functionally disable the validation of the server cert by the client if the cert in Splunk is improperly set up
-Write-Output "Sending config to Splunk"
-$body = @{
-	event =(ConvertTo-Json $orgConfig)
-}    
+Write-Output "Gathered organization config. Preparing to send to Azure Blob."
+Write-Output "Writing configuration to a blob in $($orgConfigExportsContainerName) container"
 
-$header = @{"Authorization"="Splunk " + $splunkToken}
-Invoke-RestMethod -Method Post -Uri $splunkHECEndpoint -Body (ConvertTo-Json $body) -Header $header | Out-Null # URI should be along lines of http://" + $SplunkHost + ":" + $SplunkEventCollectorPort + "/services/collector"
-Write-Output "Sent organization config to Splunk."
-#[System.Net.ServicePointManager]::ServerCertificateValidationCallback = $null
+try {
+    # Create storage context
+	$storageContext = New-AzStorageContext -ConnectionString $storageAccountConnectionString
+
+    # Create file locally to upload to blob
+    $tempPath = "$($env:TEMP)\$fileName"
+    New-Item $tempPath | Out-Null
+    Set-Content -Path $tempPath -Value (ConvertTo-Json $orgConfig)
+
+    # Upload to Blob Storage
+    Set-AzStorageBlobContent -Container $orgConfigExportsContainerName -File $tempPath -Blob $fileName -Context $storageContext | Out-Null
+
+    # Remove temp file
+    Remove-Item $tempPath -Force
+
+    Write-Output "Completed adding $fileName to Azure Storage"
+} catch {
+	Write-Error "Failed to write to Azure Blob." -Category NotSpecified
+	throw $_
+}
