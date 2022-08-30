951b04b39b854c6e4110a7303d8a9f2c4cb8fa45
John Gemkow <jogemkow@microsoft.com>
2022-08-30 18:41:28 -0500

    Updated Function script for Message Trace extraction

--- src/Function/MessageTrackingLogExtractor/run.ps1
+++ src/Function/MessageTrackingLogExtractor/run.ps1
@@ -6,16 +6,20 @@ if ($Timer.IsPastDue) {
     Write-Warning "Trigger for MessageTrackingLogExtractor is running late."
 }
 
+# Workaround for https://github.com/Azure/azure-powershell/issues/14416
+Import-Module Az.Storage -Force
+
 # Retrieve variables for Azure Function
 $certificateName = $env:CertificateName
 $clientIdSecretName = $env:ClientIdSecretName
 $keyVaultName = $env:KeyVaultName
-$splunkHECEndpointSecretName = $env:SplunkHECEndpointSecretName
-$splunkTokenSecretName = $env:SplunkMessageTraceSplunkTokenSecretName
 $tenantNameSecretName = $env:TenantNameSecretName
 $executionInterval = $env:ExecutionInterval -as [int]
-$maxNumberSplunkSendThreads = $env:MaxNumberSplunkSendThreads -as [int]
 $exoMessageTracePageSize = $env:EXOMessageTracePageSize -as [int]
+$messageTrackingExportsContainerName = $env:MessageTrackingExportsContainerName
+$storageAccountConnectionString = $env:AzureWebJobsStorage # Storage Account connection string
+
+$fileName = "EXOMessageTrace_$(get-date -f yyMMddhhmmss).json"
 
 # Retrive information from Key Vault
 try {
@@ -24,11 +28,9 @@ try {
 	$cert = Get-AzKeyVaultCertificate -VaultName $keyVaultName -Name $certificateName
 	$certValue = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $cert.Name -AsPlainText
 
-	# Obtain client ID, tenant name, Splunk Host Name, and Splunk Token from Key Vault
+	# Obtain client ID, tenant name from Key Vault
 	$clientId = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $clientIdSecretName -AsPlainText
 	$tenantName = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $tenantNameSecretName -AsPlainText
-	$splunkHECEndpoint = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $splunkHECEndpointSecretName -AsPlainText
-	$splunkToken = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $splunkTokenSecretName -AsPlainText
 	
 	Write-Verbose "Secrets and certificate retrieved."
 } catch {
@@ -40,7 +42,7 @@ try {
 try {
 	Write-Output "Connecting to EXO using AAD application service principal details from Key Vault..."
 	$certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]([System.Convert]::FromBase64String($certValue))
-	Connect-ExchangeOnline -Certificate $certificate -AppId $clientId -Organization $tenantName
+	Connect-ExchangeOnline -Certificate $certificate -AppId $clientId -Organization $tenantName -CommandName "Get-MessageTrace" -ShowBanner:$false
 	Write-Output "Connected to EXO using AAD application service principal details from Key Vault."
 } catch {
 	Write-Error -Message "Failed to connect to EXO." -Category ConnectionError
@@ -66,38 +68,32 @@ try {
 } finally {
 	Disconnect-ExchangeOnline -Confirm:$false
 }
-Write-Output "Gathered $($pageNumber-1) pages of messages / gathered $($messages.Count) messages. Preparing to send to Splunk."
+Write-Output "Gathered $($pageNumber-1) pages of messages / gathered $($messages.Count) messages."
 
-# Set up runspace pool for multi-threading
-$runspacePool = [runspacefactory]::CreateRunspacePool(1, $maxNumberSplunkSendThreads)
-$runspacePool.Open()
-$sendJobs = @()
+if ($messages) {
+    Write-Output "Gathered messages. Preparing to send to Azure Blob."
+    Write-Output "Writing traces to a blob in $($orgConfigExportsContainerName) container"
 
-# Send message by Message to HEC
-$sendScript = {
-    param($uri, $splunkToken, $message)
-    $body = @{
-		event =(ConvertTo-Json $message)
-	}    
-	
-	$header = @{"Authorization"="Splunk " + $splunkToken}
-	Invoke-RestMethod -Method Post -Uri $uri -Body (ConvertTo-Json $body) -Header $header #| Out-Null # URI should be along lines of http://" + $SplunkHost + ":" + $SplunkEventCollectorPort + "/services/collector"
+    try {
+        # Create storage context
+        $storageContext = New-AzStorageContext -ConnectionString $storageAccountConnectionString
+
+        # Create file locally to upload to blob
+        $tempPath = "$($env:TEMP)\$fileName"
+        New-Item $tempPath | Out-Null
+        Set-Content -Path $tempPath -Value (ConvertTo-Json $messages)
+
+        # Upload to Blob Storage
+        Set-AzStorageBlobContent -Container $messageTrackingExportsContainerName -File $tempPath -Blob $fileName -Context $storageContext | Out-Null
+
+        # Remove temp file
+        Remove-Item $tempPath -Force
+
+        Write-Output "Completed adding $fileName to Azure Storage"
+    } catch {
+        Write-Error "Failed to write to Azure Blob." -Category NotSpecified
+        throw $_
+    }
+} else {
+    Write-Output "Nothing to write to blob."
 }
-
-#[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true} # Functionally disable the validation of the server cert by the client if the cert in Splunk is improperly set up
-for ($i=0; $i -lt $messages.Count; $i++) {
-	Write-Output "Sending message #$($i+1) of $($messages.Count) to Splunk"
-	$m = $messages[$i]
-
-	$powerShell = [powershell]::Create()
-	$powerShell.RunspacePool = $runspacePool
-	$powerShell.AddScript($sendScript).AddArgument($splunkHECEndpoint).AddArgument($splunkToken).AddArgument($m) | Out-Null
-	$sendJobs += $powerShell.BeginInvoke()
-}
-
-while ($sendJobs.IsCompleted -contains $false) {
-	Start-Sleep 1
-}
-Write-Output "Finished sending messages to Splunk."
-#[System.Net.ServicePointManager]::ServerCertificateValidationCallback = $null
-#>
